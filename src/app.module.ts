cf4e4b708d0a8505299d82d810fe3f9788a3069a
Tâm Lê <leduytam600@gmail.com>
2023-11-29 15:04:00 +0700

    GMH-06 | add mail module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './configs/app.config';
 import mailerConfig from './configs/mailer.config';
+import { MailModule } from './mail/mail.module';
 import { MailerModule } from './mailer/mailer.module';
 
 @Module({
@@ -11,6 +12,7 @@ import { MailerModule } from './mailer/mailer.module';
       load: [appConfig, mailerConfig],
     }),
     MailerModule,
+    MailModule,
   ],
 })
 export class AppModule {}
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IAllConfig } from 'src/configs/config.interface';
+import { MailerService } from 'src/mailer/mailer.service';
+
+@Injectable()
+export class MailService {
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly configService: ConfigService<IAllConfig>,
+  ) {}
+}
