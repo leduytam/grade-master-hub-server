eb2635f58e3d4f2433436ca08cc39271cf49b37c
Lê Duy Tâm <leduytam600@gmail.com>
2024-01-07 18:14:32 +0700

    GMH-95 | Add user prop to API get replies in comment

--- src/models/classes/reviews.service.ts
+++ src/models/classes/reviews.service.ts
@@ -403,6 +403,8 @@ export class ReviewsService {
   ): Promise<Paginated<ReviewComment>> {
     const qb = this.commentsRepo
       .createQueryBuilder('comment')
+      .leftJoinAndSelect('comment.user', 'user')
+      .leftJoinAndSelect('user.avatar', 'avatar')
       .where('comment.parent = :commentId', { commentId })
       .orderBy('comment.createdAt', 'DESC');
 
